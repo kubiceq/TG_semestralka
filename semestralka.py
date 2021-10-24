18a786c790916519879403ab5b821031c9d58c3e
kubiceq <34571984+kubiceq@users.noreply.github.com>
2021-10-24 17:46:47 +0200

    upravy
    upravy

--- semestralka.py
+++ semestralka.py
@@ -14,7 +14,8 @@ except ImportError:
   print ("Trying to Install required module: matplotlib\n")
   os.system('python -m pip install -U matplotlib')
 from tabulate import tabulate
-
+import pydot
+from networkx.drawing.nx_pydot import graphviz_layout
 
 #legenda
 #???????????????????????????????? -> zadaj tvoje hodnoty
@@ -27,13 +28,15 @@ from tabulate import tabulate
 
 COLOR = "tab:blue"
 i=0
-print('#*********************************************Uloha_1**********************************************************************************')
+print('#*********************************************Uloha_1*********************************************')
+print('#*********************************************Uloha_1*********************************************')
+print('#*********************************************Uloha_1*********************************************')
+print('navod:')
+print('https://youtu.be/7fujbpJ0LB4?t=69')
 #definuj graf
 Graf_z_ulohy_1 = nx.Graph()
-# navod:
-# https://youtu.be/7fujbpJ0LB4?t=69
 
-#matica susednosti
+#matica susednosti(zadanie):
 #????????????????????????????????????????????????????????????????????
 #kubqo
 matica_susednosti = np.array([[0,0,0,0,1,0,0,0],
@@ -75,20 +78,23 @@ matica_susednosti = np.array([[0,0,0,0,1,0,0,0],
 #                               [],
 #                               []])
 #????????????????????????????????????????????????????????????????????
-# tabulate data
-table = tabulate(matica_susednosti, tablefmt="fancy_grid")
+
+# naformatuj tabulku
+table = tabulate(matica_susednosti, tablefmt="simple")
 print('************************MATICA_SUSEDNOSTI****************************')
 print(table)
+
 #vytvorenie grafu z matice susednosti
 Graf_z_ulohy_1 = nx.from_numpy_matrix(matica_susednosti)
+
 #nakresli graf z ulohy 1
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#i+=1
-#plt.figure(i)
-#plt.title('Graf z ulohy 1')
-#nx.draw(Graf_z_ulohy_1, with_labels=True, font_weight='bold')
-#plt.show()
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# i+=1
+# plt.figure(i)
+# plt.title('Graf z ulohy 1')
+# nx.draw_shell(Graf_z_ulohy_1, with_labels=True, font_weight='bold')
+# plt.show()
+# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #prehladaj do hlbky
 print("Stromy pri prehladani do hlbky")
@@ -96,12 +102,12 @@ for vrchol in range(Graf_z_ulohy_1.order()):
     Strom_hlbky = nx.dfs_tree(Graf_z_ulohy_1, source=vrchol)
     print(list(Strom_hlbky.edges()))
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#   i+=1
-#   plt.figure(vrchol+i)
-#   string = 'Strom do hlbky pre vrchol:' + str(vrchol)
-#   plt.title(string)
-#   nx.draw(Strom_hlbky, with_labels=True)
-#plt.show()
+#     i+=1
+#     plt.figure(vrchol+i)
+#     string = 'Strom do hlbky pre vrchol:' + str(vrchol)
+#     plt.title(string)
+#     nx.draw(Strom_hlbky, with_labels=True)
+# plt.show()
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #prehladaj do sirky
@@ -118,10 +124,12 @@ for vrchol in range(Graf_z_ulohy_1.order()):
 # plt.show()
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 print('#*********************************************Uloha_2**********************************************************************************')
-#navod:
-# https://blog.devgenius.io/floyd-warshall-all-pairs-shortest-path-matrix-multiplication-1ae24f3312e4
-# Pocet vrcholov, ktore dosiahnem dnes v noci
+print('#*********************************************Uloha_2**********************************************************************************')
+print('#*********************************************Uloha_2**********************************************************************************')
+print('navod:')
+print(' https://blog.devgenius.io/floyd-warshall-all-pairs-shortest-path-matrix-multiplication-1ae24f3312e4 ')
 #????????????????????????????????????????????????????????????????????
+# Pocet vrcholov, ktore dosiahnem dnes v noci
 nV = 6
 #????????????????????????????????????????????????????????????????????
 INF = 9999
@@ -187,18 +195,22 @@ G =      [[0, 3, 1, 5, INF, INF],
 floyd(G)
 
 print('#*********************************************Uloha_3**********************************************************************************')
+print('#*********************************************Uloha_3**********************************************************************************')
+print('#*********************************************Uloha_3**********************************************************************************')
 from networkx.algorithms import tree
 from networkx.drawing.nx_pydot import graphviz_layout
 Graf_z_ulohy_3 = nx.Graph()
-# navod:
-# https://www.youtube.com/watch?v=71UQH7Pr9kU
-# pekne vysvetlenie je aj na wiki
-# https://sk.wikipedia.org/wiki/Minimálna_kostra_grafu
+print('navod:')
+print(' https://www.youtube.com/watch?v=71UQH7Pr9kU')
+print(' pekne vysvetlenie je aj na wiki')
+print(' https://sk.wikipedia.org/wiki/Minimálna_kostra_grafu')
 
 
 #definuj hrany
 #????????????????????????????????????????????????????????????????????
 #kubqo
+vrcholy = ["1",'2','3','4','5','6','7','8']
+Graf_z_ulohy_3.add_nodes_from(vrcholy)
 Graf_z_ulohy_3.add_edge("1","2", weight = 6)
 Graf_z_ulohy_3.add_edge("1","3", weight = 5)
 Graf_z_ulohy_3.add_edge("1","4", weight = 1)
@@ -216,38 +228,36 @@ Graf_z_ulohy_3.add_edge("6","7", weight = 3)
 Graf_z_ulohy_3.add_edge("7","8", weight = 5)
 
 # #FLO
-# Graf_z_ulohy_3.add_edge("1","2", weight = 6)
-# Graf_z_ulohy_3.add_edge("1","3", weight = 5)
-# Graf_z_ulohy_3.add_edge("1","4", weight = 1)
-# Graf_z_ulohy_3.add_edge("2","4", weight = 4)
-# Graf_z_ulohy_3.add_edge("2","5", weight = 7)
-# Graf_z_ulohy_3.add_edge("2","7", weight = 2)
-# Graf_z_ulohy_3.add_edge("3","4", weight = 2)
-# Graf_z_ulohy_3.add_edge("3","6", weight = 8)
-# Graf_z_ulohy_3.add_edge("3","8", weight = 1)
-# Graf_z_ulohy_3.add_edge("4","5", weight = 11)
-# Graf_z_ulohy_3.add_edge("4","6", weight = 10)
-# Graf_z_ulohy_3.add_edge("4","7", weight = 9)
-# Graf_z_ulohy_3.add_edge("5","8", weight = 3)
-# Graf_z_ulohy_3.add_edge("6","7", weight = 3)
-# Graf_z_ulohy_3.add_edge("7","8", weight = 5)
+# Graf_z_ulohy_3.add_edge("1","2", weight = 2)
+# Graf_z_ulohy_3.add_edge("1","7", weight = 5)
+# Graf_z_ulohy_3.add_edge("1","8", weight = 10)
+# Graf_z_ulohy_3.add_edge("2","3", weight = 10)
+# Graf_z_ulohy_3.add_edge("2","7", weight = 6)
+# Graf_z_ulohy_3.add_edge("3","4", weight = 5)
+# Graf_z_ulohy_3.add_edge("3","8", weight = 4)
+# Graf_z_ulohy_3.add_edge("4","5", weight = 8)
+# Graf_z_ulohy_3.add_edge("4","8", weight = 3)
+# Graf_z_ulohy_3.add_edge("5","6", weight = 3)
+# Graf_z_ulohy_3.add_edge("5","8", weight = 7)
+# Graf_z_ulohy_3.add_edge("6","7", weight = 11)
+# Graf_z_ulohy_3.add_edge("6","8", weight = 9)
+# Graf_z_ulohy_3.add_edge("7","8", weight = 12)
 
 # #LJ
-# Graf_z_ulohy_3.add_edge("1","2", weight = 6)
-# Graf_z_ulohy_3.add_edge("1","3", weight = 5)
-# Graf_z_ulohy_3.add_edge("1","4", weight = 1)
-# Graf_z_ulohy_3.add_edge("2","4", weight = 4)
-# Graf_z_ulohy_3.add_edge("2","5", weight = 7)
-# Graf_z_ulohy_3.add_edge("2","7", weight = 2)
-# Graf_z_ulohy_3.add_edge("3","4", weight = 2)
-# Graf_z_ulohy_3.add_edge("3","6", weight = 8)
-# Graf_z_ulohy_3.add_edge("3","8", weight = 1)
-# Graf_z_ulohy_3.add_edge("4","5", weight = 11)
-# Graf_z_ulohy_3.add_edge("4","6", weight = 10)
-# Graf_z_ulohy_3.add_edge("4","7", weight = 9)
-# Graf_z_ulohy_3.add_edge("5","8", weight = 3)
-# Graf_z_ulohy_3.add_edge("6","7", weight = 3)
+# Graf_z_ulohy_3.add_edge("1","2", weight = 5)
+# Graf_z_ulohy_3.add_edge("1","3", weight = 4)
+# Graf_z_ulohy_3.add_edge("2","3", weight = 8)
+# Graf_z_ulohy_3.add_edge("2","4", weight = 2)
+# Graf_z_ulohy_3.add_edge("2","7", weight = 7)
+# Graf_z_ulohy_3.add_edge("3","4", weight = 3)
+# Graf_z_ulohy_3.add_edge("3","5", weight = 3)
+# Graf_z_ulohy_3.add_edge("4","5", weight = 9)
+# Graf_z_ulohy_3.add_edge("4","6", weight = 6)
+# Graf_z_ulohy_3.add_edge("4","7", weight = 1)
+# Graf_z_ulohy_3.add_edge("6","7", weight = 8)
+# Graf_z_ulohy_3.add_edge("6","8", weight = 7)
 # Graf_z_ulohy_3.add_edge("7","8", weight = 5)
+
 #????????????????????????????????????????????????????????????????????
 
 #nakresli graf 3
@@ -255,7 +265,7 @@ Graf_z_ulohy_3.add_edge("7","8", weight = 5)
 # i+=1
 # plt.figure(i)
 # plt.title('Graf 3')
-# pos=nx.spring_layout(Graf_z_ulohy_3) # pos = nx.nx_agraph.graphviz_layout(Graf_z_ulohy_3)
+# pos=nx.circular_layout(Graf_z_ulohy_3) # pos = nx.nx_agraph.graphviz_layout(Graf_z_ulohy_3)
 # nx.draw_networkx(Graf_z_ulohy_3,pos)
 # labels = nx.get_edge_attributes(Graf_z_ulohy_3,'weight')
 # nx.draw_networkx_edge_labels(Graf_z_ulohy_3,pos,edge_labels=labels)
@@ -272,26 +282,26 @@ print(sorted(edgelist))
 
 #vykresli minimalny strom grafu 3
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#Tmin = nx.Graph();
-#Tmin.add_edges_from(edgelist)
-#i+=1
-#plt.figure(i)
-#plt.title('Minimalna kostra grafu 3')
-#nx.draw(Tmin, with_labels=True)
-#plt.show()
+# Tmin = nx.Graph();
+# Tmin.add_edges_from(edgelist)
+# i+=1
+# plt.figure(i)
+# plt.title('Minimalna kostra grafu 3')
+# nx.draw(Tmin, with_labels=True)
+# plt.show()
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #vykresli minimalny strom V grafe 3
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# i+=1
-# plt.figure(i)
-# plt.title('Graf 3')
-# pos=nx.spring_layout(Minimalny strom v grafe 3)
-# nx.draw_networkx(Graf_z_ulohy_3,pos)
-# labels = nx.get_edge_attributes(Graf_z_ulohy_3,'weight')
-# nx.draw_networkx_edges(Graf_z_ulohy_3,pos,edgelist=edgelist,edge_color= COLOR,width=4)
-# nx.draw_networkx_edge_labels(Graf_z_ulohy_3,pos,edge_labels=labels)
-# plt.show()
+i+=1
+plt.figure(i)
+plt.title('Minimalny strom v Grafe 3')
+pos=nx.shell_layout(Graf_z_ulohy_3)
+nx.draw_networkx(Graf_z_ulohy_3,pos)
+labels = nx.get_edge_attributes(Graf_z_ulohy_3,'weight')
+nx.draw_networkx_edges(Graf_z_ulohy_3,pos,edgelist=edgelist,edge_color= COLOR,width=4)
+nx.draw_networkx_edge_labels(Graf_z_ulohy_3,pos,edge_labels=labels)
+plt.show()
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -328,13 +338,15 @@ print(sorted(edgelist))
 
 
 print('#*********************************************Uloha_4**********************************************************************************')
+print('#*********************************************Uloha_4**********************************************************************************')
+print('#*********************************************Uloha_4**********************************************************************************')
 Graf_z_ulohy_4 = nx.DiGraph()
 
 #definuj hrany
 #z - zdroj
 #u - ustie
-# navod:
-# https://www.youtube.com/watch?v=Tl90tNtKvxs
+print(' navod:')
+print(' https://www.youtube.com/watch?v=Tl90tNtKvxs ')
 #????????????????????????????????????????????????????????????????????
 #kubqo
 Graf_z_ulohy_4.add_edge("z","1", capacity = 14)
@@ -353,36 +365,39 @@ Graf_z_ulohy_4.add_edge("6","2", capacity = 9)
 Graf_z_ulohy_4.add_edge("6","u", capacity = 11)
 
 # #FLO
-# Graf_z_ulohy_4.add_edge("z","1", capacity = 14)
-# Graf_z_ulohy_4.add_edge("z","2", capacity = 15)
-# Graf_z_ulohy_4.add_edge("z","3", capacity = 13)
-# Graf_z_ulohy_4.add_edge("1","2", capacity = 6)
-# Graf_z_ulohy_4.add_edge("1","4", capacity = 4)
-# Graf_z_ulohy_4.add_edge("1","5", capacity = 8)
-# Graf_z_ulohy_4.add_edge("2","5", capacity = 4)
+# Graf_z_ulohy_4.add_edge("z","1", capacity = 8)
+# Graf_z_ulohy_4.add_edge("z","2", capacity = 5)
+# Graf_z_ulohy_4.add_edge("z","3", capacity = 2)
+# Graf_z_ulohy_4.add_edge("1","4", capacity = 2)
+# Graf_z_ulohy_4.add_edge("2","4", capacity = 3)
+# Graf_z_ulohy_4.add_edge("2","5", capacity = 3)
 # Graf_z_ulohy_4.add_edge("2","3", capacity = 3)
-# Graf_z_ulohy_4.add_edge("2","u", capacity = 10)
-# Graf_z_ulohy_4.add_edge("3","6", capacity = 8)
-# Graf_z_ulohy_4.add_edge("4","u", capacity = 9)
-# Graf_z_ulohy_4.add_edge("5","4", capacity = 7)
-# Graf_z_ulohy_4.add_edge("6","2", capacity = 9)
-# Graf_z_ulohy_4.add_edge("6","u", capacity = 11)
+# Graf_z_ulohy_4.add_edge("3","6", capacity = 5)
+# Graf_z_ulohy_4.add_edge("3","u", capacity = 2)
+# Graf_z_ulohy_4.add_edge("4","u", capacity = 2)
+# Graf_z_ulohy_4.add_edge("5","1", capacity = 5)
+# Graf_z_ulohy_4.add_edge("5","4", capacity = 3)
+# Graf_z_ulohy_4.add_edge("5","6", capacity = 5)
+# Graf_z_ulohy_4.add_edge("5","u", capacity = 3)
+# Graf_z_ulohy_4.add_edge("6","u", capacity = 10)
 
 # #LJ
-# Graf_z_ulohy_4.add_edge("z","1", capacity = 14)
-# Graf_z_ulohy_4.add_edge("z","2", capacity = 15)
-# Graf_z_ulohy_4.add_edge("z","3", capacity = 13)
-# Graf_z_ulohy_4.add_edge("1","2", capacity = 6)
-# Graf_z_ulohy_4.add_edge("1","4", capacity = 4)
-# Graf_z_ulohy_4.add_edge("1","5", capacity = 8)
-# Graf_z_ulohy_4.add_edge("2","5", capacity = 4)
+# Graf_z_ulohy_4.add_edge("z","1", capacity = 6)
+# Graf_z_ulohy_4.add_edge("z","4", capacity = 15)
+# Graf_z_ulohy_4.add_edge("z","6", capacity = 5)
+# Graf_z_ulohy_4.add_edge("1","2", capacity = 8)
 # Graf_z_ulohy_4.add_edge("2","3", capacity = 3)
-# Graf_z_ulohy_4.add_edge("2","u", capacity = 10)
-# Graf_z_ulohy_4.add_edge("3","6", capacity = 8)
-# Graf_z_ulohy_4.add_edge("4","u", capacity = 9)
-# Graf_z_ulohy_4.add_edge("5","4", capacity = 7)
-# Graf_z_ulohy_4.add_edge("6","2", capacity = 9)
-# Graf_z_ulohy_4.add_edge("6","u", capacity = 11)
+# Graf_z_ulohy_4.add_edge("2","5", capacity = 5)
+# Graf_z_ulohy_4.add_edge("2","u", capacity = 4)
+# Graf_z_ulohy_4.add_edge("3","u", capacity = 9)
+# Graf_z_ulohy_4.add_edge("4","1", capacity = 6)
+# Graf_z_ulohy_4.add_edge("4","2", capacity = 3)
+# Graf_z_ulohy_4.add_edge("4","5", capacity = 3)
+# Graf_z_ulohy_4.add_edge("4","6", capacity = 3)
+# Graf_z_ulohy_4.add_edge("5","3", capacity = 3)
+# Graf_z_ulohy_4.add_edge("5","6", capacity = 3)
+# Graf_z_ulohy_4.add_edge("5","u", capacity = 5)
+# Graf_z_ulohy_4.add_edge("6","u", capacity = 10)
 
 #????????????????????????????????????????????????????????????????????
 
@@ -409,7 +424,6 @@ print('Kapacita maximalneho toku: ',kapacita)
 # edgelist = []
 # edgeLabels = {}
 # for vrchol in flow_dict:
-#     #print(flow_dict[vrchol])
 #     for protiVrchol in flow_dict[vrchol]:
 #         edgelist.append([vrchol,protiVrchol])
 #         edgeLabels[(vrchol,protiVrchol)] = flow_dict[vrchol][protiVrchol]
