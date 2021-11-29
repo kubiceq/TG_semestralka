064d6ce4d106b4aa6813ec6c13f25328e7b34152
kubiceq <34571984+kubiceq@users.noreply.github.com>
2021-11-29 18:56:44 +0100

    LJ
    final LJ

--- semestralka.py
+++ semestralka.py
@@ -10,13 +10,17 @@ from networkx.drawing.nx_pydot import graphviz_layout
 #???????????????????????????????? -> zadaj tvoje hodnoty
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -> odkomentuj pre grafy
 # pre odkomentovanie bloku textu oznac a stlac 'Ctrl' + '/'
-
-
-
-
-
+#Disclaimer:
+# ak sa ti nahodou niekedy tento krasny kus kodu dostane pod ruky, vedz, ze si nim mozes pracu odkontrolovat, ale rozhodne vsetko neurobi za teba.
+#Vacsina vypisov musi v konecnom dosledku vyzerat inak, ako nam ju vypise skript
+#Global(and needed) variables
 COLOR = "tab:blue"
 i=0
+#####
+
+
+
+
 print('#*********************************************Uloha_1*********************************************')
 print('#*********************************************Uloha_1*********************************************')
 print('#*********************************************Uloha_1*********************************************')
@@ -28,25 +32,25 @@ Graf_z_ulohy_1 = nx.DiGraph()
 #matica susednosti(zadanie):
 #????????????????????????????????????????????????????????????????????
 
-#kubqo
-matica_susednosti = np.matrix([[0,0,0,0,1,0,0,0],
-                               [0,0,0,1,0,1,0,0],
-                               [1,0,0,0,0,1,0,1],
-                               [0,0,0,0,0,0,0,1],
-                               [0,0,1,1,0,0,0,1],
-                               [0,0,0,1,0,0,1,1],
-                               [0,1,0,1,1,0,0,0],
-                               [1,0,0,0,0,0,0,0]])
+# #kubqo
+# matica_susednosti = np.matrix([[0,0,0,0,1,0,0,0],
+#                                [0,0,0,1,0,1,0,0],
+#                                [1,0,0,0,0,1,0,1],
+#                                [0,0,0,0,0,0,0,1],
+#                                [0,0,1,1,0,0,0,1],
+#                                [0,0,0,1,0,0,1,1],
+#                                [0,1,0,1,1,0,0,0],
+#                                [1,0,0,0,0,0,0,0]])
 
 # #FLO
-# matica_susednosti = np.array([[0,0,0,1,1,0,1,1],
-#                               [0,0,1,0,1,1,1,0],
-#                               [0,1,0,0,0,1,1,0],
-#                               [1,0,0,0,1,0,0,1],
-#                               [1,1,0,1,0,1,0,1],
-#                               [0,1,1,0,1,0,0,1],
-#                               [1,1,1,0,0,0,0,1],
-#                               [1,0,0,1,1,1,1,0]])
+matica_susednosti = np.array([[0,0,0,1,1,0,1,1],
+                              [0,0,1,0,1,1,1,0],
+                              [0,1,0,0,0,1,1,0],
+                              [1,0,0,0,1,0,0,1],
+                              [1,1,0,1,0,1,0,1],
+                              [0,1,1,0,1,0,0,1],
+                              [1,1,1,0,0,0,0,1],
+                              [1,0,0,1,1,1,1,0]])
 
 # #LJ
 # matica_susednosti = np.array([[0,1,0,0,1,0,1,0],
@@ -99,6 +103,7 @@ for vrchol in range(Graf_z_ulohy_1.order()):
         hrana[1] += 1
         hrany_od_1.append(hrana)
     print(hrany_od_1)
+#Vykresli vsetky stromy
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #     i+=1
 #     edgelist = []
@@ -123,6 +128,7 @@ for vrchol in range(Graf_z_ulohy_1.order()):
         hrana[1] += 1
         hrany_od_1.append(hrana)
     print(hrany_od_1)
+#Vykresli vsetky stromy
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #     i += 1
 #     edgelist = []
@@ -146,7 +152,6 @@ print(' https://blog.devgenius.io/floyd-warshall-all-pairs-shortest-path-matrix-
 #????????????????????????????????????????????????????????????????????
 #INF je pre nas nekonecno. preto ak bude v maticiach nejake velke cislo(blizke nasej INF), bude to pravdepodobne nekonecno
 INF = 9999
-print('Zadanie == Stupen 0')
 # Algorithm
 def floyd(G):
     dist = list(map(lambda p: list(map(lambda q: q, p)), G))
@@ -166,7 +171,7 @@ def sol(dist):
 #Cenova Matica
 #????????????????????????????????????????????????????????????????????
 # #cvicenie 8 prva matica
-# Pocet vrcholov, ktore dosiahnem dnes v noci
+#nV => Pocet vrcholov, ktore dosiahnem dnes v noci
 #nV = 7
 # G =      [[0, 2, 4, 8, 7, 8, INF],
 #          [INF, 0, 1, INF, INF, INF,INF],
@@ -177,16 +182,16 @@ def sol(dist):
 #          [INF,INF,INF,INF,INF,6,0]]
 
 #cvicenie 8 druha matica
-# Pocet vrcholov, ktore dosiahnem dnes v noci
-nV = 5
-G =      [[0,9,5,INF,7],
-         [9,0,11,3,2],
-         [5,11,0,6,7],
-         [INF,3,6,0,4],
-         [7,2,7,4,0]]
+#nV => Pocet vrcholov, ktore dosiahnem dnes v noci
+# nV = 5
+# G =      [[0,9,5,INF,7],
+#          [9,0,11,3,2],
+#          [5,11,0,6,7],
+#          [INF,3,6,0,4],
+#          [7,2,7,4,0]]
 
 # #kubqo
-# Pocet vrcholov, ktore dosiahnem dnes v noci
+#nV => Pocet vrcholov, ktore dosiahnem dnes v noci
 #nV = 6
 # G =      [[0, 3, 1, 5, INF, INF],
 #          [3, 0, 4, INF, INF, 2],
@@ -195,19 +200,19 @@ G =      [[0,9,5,INF,7],
 #          [INF, INF, 7, 3, 0, 5 ],
 #          [INF, 2, INF, INF, 5, 0]]
 
-# #FLO
-# Pocet vrcholov, ktore dosiahnem dnes v noci
-#nV = 6
-# G =      [[0,9,INF,INF,INF,4],
-#          [INF,0,11,7,2,INF],
-#          [5,8,0,INF,INF,6],
-#          [INF,INF,6,0,4,INF],
-#          [7,INF,7,INF,0,3],
-#          [INF,2,INF,10,9,0]]
+#FLO
+# #nV => Pocet vrcholov, ktore dosiahnem dnes v noci
+nV = 6
+G =      [[0,9,INF,INF,INF,4],
+         [INF,0,11,7,2,INF],
+         [5,8,0,INF,INF,6],
+         [INF,INF,6,0,4,INF],
+         [7,INF,7,INF,0,3],
+         [INF,2,INF,10,9,0]]
 
 #LJ
-# Pocet vrcholov, ktore dosiahnem dnes v noci
-#nV = 6
+#nV => Pocet vrcholov, ktore dosiahnem dnes v noci
+# nV = 6
 # G =      [[0,1,2,INF,INF,6],
 #          [INF,0,3,4,8,INF],
 #          [INF,INF,0,9,5,1],
@@ -216,7 +221,8 @@ G =      [[0,9,5,INF,7],
 #          [INF,10,INF,INF,INF,0]]
 
 # #ty
-# Pocet vrcholov, ktore dosiahnem dnes v noci
+#nV => Pocet vrcholov, ktore dosiahnem dnes v noci
+#???????????????????????????????????????????????????????????????????
 #nV =
 # G =      [[],
 #          [],
@@ -225,6 +231,7 @@ G =      [[0,9,5,INF,7],
 #          [],
 #          []]
 #????????????????????????????????????????????????????????????????????
+
 #matica z navodu
 #navod:
 # https://blog.devgenius.io/floyd-warshall-all-pairs-shortest-path-matrix-multiplication-1ae24f3312e4
@@ -232,6 +239,9 @@ G =      [[0,9,5,INF,7],
 #          [INF,0,INF,9],
 #          [-2,4,0,INF],
 #          [INF,-3,1,0]]
+print('Zadanie == Stupen 0')
+dist = list(map(lambda p: list(map(lambda q: q, p)), G))
+sol(dist)
 floyd(G)
 
 
@@ -248,24 +258,24 @@ Graf_z_ulohy_3 = nx.Graph()
 
 #definuj hrany
 #????????????????????????????????????????????????????????????????????
-#kubqo
-vrcholy = ["1",'2','3','4','5','6','7','8']
-Graf_z_ulohy_3.add_nodes_from(vrcholy)
-Graf_z_ulohy_3.add_edge("1","2", weight = 6)
-Graf_z_ulohy_3.add_edge("1","3", weight = 5)
-Graf_z_ulohy_3.add_edge("1","4", weight = 1)
-Graf_z_ulohy_3.add_edge("2","4", weight = 4)
-Graf_z_ulohy_3.add_edge("2","5", weight = 7)
-Graf_z_ulohy_3.add_edge("2","7", weight = 2)
-Graf_z_ulohy_3.add_edge("3","4", weight = 2)
-Graf_z_ulohy_3.add_edge("3","6", weight = 8)
-Graf_z_ulohy_3.add_edge("3","8", weight = 1)
-Graf_z_ulohy_3.add_edge("4","5", weight = 11)
-Graf_z_ulohy_3.add_edge("4","6", weight = 10)
-Graf_z_ulohy_3.add_edge("4","7", weight = 9)
-Graf_z_ulohy_3.add_edge("5","8", weight = 3)
-Graf_z_ulohy_3.add_edge("6","7", weight = 3)
-Graf_z_ulohy_3.add_edge("7","8", weight = 5)
+# #kubqo
+# vrcholy = ["1",'2','3','4','5','6','7','8']
+# Graf_z_ulohy_3.add_nodes_from(vrcholy)
+# Graf_z_ulohy_3.add_edge("1","2", weight = 6)
+# Graf_z_ulohy_3.add_edge("1","3", weight = 5)
+# Graf_z_ulohy_3.add_edge("1","4", weight = 1)
+# Graf_z_ulohy_3.add_edge("2","4", weight = 4)
+# Graf_z_ulohy_3.add_edge("2","5", weight = 7)
+# Graf_z_ulohy_3.add_edge("2","7", weight = 2)
+# Graf_z_ulohy_3.add_edge("3","4", weight = 2)
+# Graf_z_ulohy_3.add_edge("3","6", weight = 8)
+# Graf_z_ulohy_3.add_edge("3","8", weight = 1)
+# Graf_z_ulohy_3.add_edge("4","5", weight = 11)
+# Graf_z_ulohy_3.add_edge("4","6", weight = 10)
+# Graf_z_ulohy_3.add_edge("4","7", weight = 9)
+# Graf_z_ulohy_3.add_edge("5","8", weight = 3)
+# Graf_z_ulohy_3.add_edge("6","7", weight = 3)
+# Graf_z_ulohy_3.add_edge("7","8", weight = 5)
 
 # #FLO
 # Graf_z_ulohy_3.add_edge("1","2", weight = 2)
@@ -284,19 +294,19 @@ Graf_z_ulohy_3.add_edge("7","8", weight = 5)
 # Graf_z_ulohy_3.add_edge("7","8", weight = 12)
 
 # #LJ
-# Graf_z_ulohy_3.add_edge("1","2", weight = 5)
-# Graf_z_ulohy_3.add_edge("1","3", weight = 4)
-# Graf_z_ulohy_3.add_edge("2","3", weight = 8)
-# Graf_z_ulohy_3.add_edge("2","4", weight = 2)
-# Graf_z_ulohy_3.add_edge("2","7", weight = 7)
-# Graf_z_ulohy_3.add_edge("3","4", weight = 3)
-# Graf_z_ulohy_3.add_edge("3","5", weight = 3)
-# Graf_z_ulohy_3.add_edge("4","5", weight = 9)
-# Graf_z_ulohy_3.add_edge("4","6", weight = 6)
-# Graf_z_ulohy_3.add_edge("4","7", weight = 1)
-# Graf_z_ulohy_3.add_edge("6","7", weight = 8)
-# Graf_z_ulohy_3.add_edge("6","8", weight = 7)
-# Graf_z_ulohy_3.add_edge("7","8", weight = 5)
+Graf_z_ulohy_3.add_edge("1","2", weight = 5)
+Graf_z_ulohy_3.add_edge("1","3", weight = 4)
+Graf_z_ulohy_3.add_edge("2","3", weight = 8)
+Graf_z_ulohy_3.add_edge("2","4", weight = 2)
+Graf_z_ulohy_3.add_edge("2","7", weight = 7)
+Graf_z_ulohy_3.add_edge("3","4", weight = 3)
+Graf_z_ulohy_3.add_edge("3","5", weight = 3)
+Graf_z_ulohy_3.add_edge("4","5", weight = 9)
+Graf_z_ulohy_3.add_edge("4","6", weight = 6)
+Graf_z_ulohy_3.add_edge("4","7", weight = 1)
+Graf_z_ulohy_3.add_edge("6","7", weight = 8)
+Graf_z_ulohy_3.add_edge("6","8", weight = 7)
+Graf_z_ulohy_3.add_edge("7","8", weight = 5)
 
 #????????????????????????????????????????????????????????????????????
 
@@ -389,20 +399,20 @@ print(' navod:')
 print(' https://www.youtube.com/watch?v=Tl90tNtKvxs ')
 #????????????????????????????????????????????????????????????????????
 #kubqo
-Graf_z_ulohy_4.add_edge("z","1", capacity = 14)
-Graf_z_ulohy_4.add_edge("z","2", capacity = 15)
-Graf_z_ulohy_4.add_edge("z","3", capacity = 13)
-Graf_z_ulohy_4.add_edge("1","2", capacity = 6)
-Graf_z_ulohy_4.add_edge("1","4", capacity = 4)
-Graf_z_ulohy_4.add_edge("1","5", capacity = 8)
-Graf_z_ulohy_4.add_edge("2","5", capacity = 4)
-Graf_z_ulohy_4.add_edge("2","3", capacity = 3)
-Graf_z_ulohy_4.add_edge("2","u", capacity = 10)
-Graf_z_ulohy_4.add_edge("3","6", capacity = 8)
-Graf_z_ulohy_4.add_edge("4","u", capacity = 9)
-Graf_z_ulohy_4.add_edge("5","4", capacity = 7)
-Graf_z_ulohy_4.add_edge("6","2", capacity = 9)
-Graf_z_ulohy_4.add_edge("6","u", capacity = 11)
+# Graf_z_ulohy_4.add_edge("z","1", capacity = 14)
+# Graf_z_ulohy_4.add_edge("z","2", capacity = 15)
+# Graf_z_ulohy_4.add_edge("z","3", capacity = 13)
+# Graf_z_ulohy_4.add_edge("1","2", capacity = 6)
+# Graf_z_ulohy_4.add_edge("1","4", capacity = 4)
+# Graf_z_ulohy_4.add_edge("1","5", capacity = 8)
+# Graf_z_ulohy_4.add_edge("2","5", capacity = 4)
+# Graf_z_ulohy_4.add_edge("2","3", capacity = 3)
+# Graf_z_ulohy_4.add_edge("2","u", capacity = 10)
+# Graf_z_ulohy_4.add_edge("3","6", capacity = 8)
+# Graf_z_ulohy_4.add_edge("4","u", capacity = 9)
+# Graf_z_ulohy_4.add_edge("5","4", capacity = 7)
+# Graf_z_ulohy_4.add_edge("6","2", capacity = 9)
+# Graf_z_ulohy_4.add_edge("6","u", capacity = 11)
 
 # #FLO
 # Graf_z_ulohy_4.add_edge("z","1", capacity = 8)
@@ -422,22 +432,22 @@ Graf_z_ulohy_4.add_edge("6","u", capacity = 11)
 # Graf_z_ulohy_4.add_edge("6","u", capacity = 10)
 
 # #LJ
-# Graf_z_ulohy_4.add_edge("z","1", capacity = 6)
-# Graf_z_ulohy_4.add_edge("z","4", capacity = 15)
-# Graf_z_ulohy_4.add_edge("z","6", capacity = 5)
-# Graf_z_ulohy_4.add_edge("1","2", capacity = 8)
-# Graf_z_ulohy_4.add_edge("2","3", capacity = 3)
-# Graf_z_ulohy_4.add_edge("2","5", capacity = 5)
-# Graf_z_ulohy_4.add_edge("2","u", capacity = 4)
-# Graf_z_ulohy_4.add_edge("3","u", capacity = 9)
-# Graf_z_ulohy_4.add_edge("4","1", capacity = 6)
-# Graf_z_ulohy_4.add_edge("4","2", capacity = 3)
-# Graf_z_ulohy_4.add_edge("4","5", capacity = 3)
-# Graf_z_ulohy_4.add_edge("4","6", capacity = 3)
-# Graf_z_ulohy_4.add_edge("5","3", capacity = 3)
-# Graf_z_ulohy_4.add_edge("5","6", capacity = 3)
-# Graf_z_ulohy_4.add_edge("5","u", capacity = 5)
-# Graf_z_ulohy_4.add_edge("6","u", capacity = 10)
+Graf_z_ulohy_4.add_edge("z","1", capacity = 6)
+Graf_z_ulohy_4.add_edge("z","4", capacity = 15)
+Graf_z_ulohy_4.add_edge("z","6", capacity = 5)
+Graf_z_ulohy_4.add_edge("1","2", capacity = 8)
+Graf_z_ulohy_4.add_edge("2","3", capacity = 3)
+Graf_z_ulohy_4.add_edge("2","5", capacity = 5)
+Graf_z_ulohy_4.add_edge("2","u", capacity = 4)
+Graf_z_ulohy_4.add_edge("3","u", capacity = 9)
+Graf_z_ulohy_4.add_edge("4","1", capacity = 6)
+Graf_z_ulohy_4.add_edge("4","2", capacity = 3)
+Graf_z_ulohy_4.add_edge("4","5", capacity = 3)
+Graf_z_ulohy_4.add_edge("4","6", capacity = 3)
+Graf_z_ulohy_4.add_edge("5","3", capacity = 3)
+Graf_z_ulohy_4.add_edge("5","6", capacity = 3)
+Graf_z_ulohy_4.add_edge("5","u", capacity = 5)
+Graf_z_ulohy_4.add_edge("6","u", capacity = 10)
 
 #????????????????????????????????????????????????????????????????????
 
@@ -455,9 +465,6 @@ Graf_z_ulohy_4.add_edge("6","u", capacity = 11)
 kapacita, flow_dict = nx.maximum_flow(Graf_z_ulohy_4,"z","u")
 print("Maximalny tok:",flow_dict)
 print('Kapacita maximalneho toku: ',kapacita)
-
-
-
 
 #vykresli maximalny tok v grafe 4
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -500,12 +507,12 @@ print('Kapacita minimalneho rezu: ',kapacita)
 #         for vrchol in skupina:
 #             index = list(Graf_z_ulohy_4).index(vrchol)
 #             color_map[index] = 'blue'
-#             print(index)
+#             #print(index)
 #     if pom == 2:
 #         for vrchol in skupina:
 #             index = list(Graf_z_ulohy_4).index(vrchol)
 #             color_map[index] = 'red'
-#             print(index)
+#             #print(index)
 # plt.title('Rozdelenie grafu pre minimalny rez')
 # pos=nx.planar_layout(Graf_z_ulohy_4)
 # labels = nx.get_edge_attributes(Graf_z_ulohy_4,'capacity')
